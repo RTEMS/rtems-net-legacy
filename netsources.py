2cea2ccc9522ebc7a21f55a63ece254d12b512e0
Ranulfo Raphael <raanulfo@hotmail.com>
2025-01-06 13:37:00 -0300

    netsources.py: fix rtemsdialer.h path
    The rtemsdialer.h is under include/rtems/ directory.
    
    Closes #9

--- netsources.py
+++ netsources.py
@@ -312,7 +312,7 @@ header = {
         'rtems/rtems_bsdnet_internal.h', 'rtems/rtems_dhcp_failsafe.h',
         'rtems/rtems_mii_ioctl.h', 'rtems/rtems_netdb.h',
         'rtems/rtems_netinet_in.h', 'rtems/rtems_syscall.h',
-        'rtems/rtemsdialer.h'
+        'include/rtems/rtemsdialer.h'
     ],
     'rtems/bsd': [
         'include/rtems/bsd/iface.h',
